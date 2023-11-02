321094f3fd7038d89b31bc5abb4d05f14fa1ff64
Iman Haqim <imanhqm@protonmail.com>
2023-11-02 21:58:53 +0800

    Add button to create new grid with custom dimension

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 let mainContainer = document.getElementById('mainContainer');
 let gridContainer = document.getElementById('gridContainer');
+let newGridBtn = document.getElementById('newGridBtn');
+
+newGridBtn.addEventListener('click', ()=> {
+    let dimension = prompt('Enter the size of your square grid (e.g., 8 for an 8x8 grid)');
+    
+    //check if dimension is a number
+    if (isNaN(dimension)) {
+        alert('Input is not a number (eg, 8)');
+
+    } else {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.lastChild);
+          }
+        
+        createGrid(dimension);
+    }
+    
+})
 
 gridContainer.addEventListener('mouseover', (event)=> {
     event.target.style.backgroundColor = 'blue';
 })
 
-//loop for 16 rows
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.id = 'gridRow';
-
-    //create 16 boxes
-    for (let i = 0; i < 16; i++) {
-        let box = document.createElement('div');
-        box.id = 'gridBox'
-        row.append(box);
-
-        console.log(`Box ${i} was created`);
+function createGrid(dimension) {
+    for (let i = 0; i < dimension; i++) {
+        let row = document.createElement('div');
+        row.id = 'gridRow';
+    
+        //create 16 boxes
+        for (let i = 0; i < dimension; i++) {
+            let box = document.createElement('div');
+            box.id = 'gridBox'
+            row.append(box);
+    
+            console.log(`Box ${i} was created`);
+        }
+    
+        gridContainer.append(row);
     }
-
-    gridContainer.append(row);
 }
+
+createGrid(16);
+
+
   
